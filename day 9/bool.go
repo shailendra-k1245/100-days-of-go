3372b215e37006ed087ea21e83bb4d7b9872d579
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2023-07-06 15:20:11 +0530

    datatypes

---
+++ day 9/bool.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var b1 bool = true
+	var b2 = true
+	var b3 bool
+	b4 := true
+
+	fmt.Println(b1) // Returns true
+	fmt.Println(b2) // Returns true
+	fmt.Println(b3) // Returns false
+	fmt.Println(b4) // Returns true
+
+}
