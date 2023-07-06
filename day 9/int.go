2e6767a3257fac81d407cab194a96bbf5945c6da
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2023-07-06 18:09:15 +0530

    unsigned int

--- day 9/int.go
+++ day 9/int.go
@@ -10,12 +10,12 @@ func main() {
 	var y int = -4500
 
 	// unsigned integer
-	var i unit = 600
-	var j unit = 4600
+	var i uint = 600
+	var j uint = 4600
 
 	fmt.Printf("Type : %T, value: %v \n", x, x)
 	fmt.Printf("Type: %T, value: %v \n", y, y)
 
-	fmt.Printf("Type: %T, value: %v", x, x)
-	fmt.Printf("Type: %T, value: %v", y, y)
+	fmt.Printf("Type: %T, value: %v \n", i, i)
+	fmt.Printf("Type: %T, value: %v", j, j)
 }
